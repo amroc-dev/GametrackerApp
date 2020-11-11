78b0be46c891e7c38ad737315b7aacb7612efc08
Marco <>
2020-11-11 13:14:17 +0000

    package upgrade

--- src/MenuStackNavigator.js
+++ src/MenuStackNavigator.js
@@ -7,9 +7,8 @@ import SearchStackNavigator from "./SearchStackNavigator";
 const Drawer = createDrawerNavigator();
 
 export default function MenuStackNavigator() {
-
   return (
-    <Drawer.Navigator drawerType="back" edgeWidth={0}>
+    <Drawer.Navigator drawerType="back" edgeWidth={0} screenOptions={{ headerShown: false }}>
       <Drawer.Screen name="Search" component={SearchStackNavigator} />
     </Drawer.Navigator>
   );
